49d925762adc93627a31ef8852c541767eea391c
Averkova Anastasia <55045356+averkova-a@users.noreply.github.com>
2020-07-25 18:23:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+import { CharactersView } from './src/charactersView.js';
+import { sortByDateAsc, sortByDateDesc, sortByEpisodesAsc, sortByEpisodesDesc} from './src/sorting.js';
+import { trackScroll, hideCards} from './src/cardDisplay.js';
+import './style.css'
+
+const url = 'https://rickandmortyapi.com/api/character';
+
+document.querySelector('button#sort-ep-asc').onclick = function () {
+    sortByEpisodesAsc();
+}
+document.querySelector('button#sort-ep-desc').onclick = function () {
+    sortByEpisodesDesc();
+}
+document.querySelector('button#sort-date-asc').onclick = function () {
+    sortByDateAsc();
+}
+document.querySelector('button#sort-date-desc').onclick = function () {
+    sortByDateDesc();
+}
+document.querySelector('button#display').onclick = function () {
+    hideCards();
+}
+
+document.addEventListener("click", function(event) {
+    if(event.target.matches('button.delete')) {
+        const cardID = event.target.className.split(' ')[0];
+        this.querySelector('.character.'+cardID).remove();
+        this.querySelector("[style='display: none;']").style.display = '';
+    }
+});
+
+window.addEventListener('scroll', trackScroll);
+
+
+function callApi(url) {
+    return fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            return data.results; // массив объектов
+        })
+        .catch((error) => {
+            console.log(JSON.stringify(error));
+        });
+};
+
+
+class App {
+    constructor() {
+      this.startApp();
+    }
+  
+    static rootElement = document.getElementById('root');
+  
+    async startApp() {
+      try {
+        
+        const characters = await callApi(url);
+        const charactersView = new CharactersView(characters);
+        const charactersElement = charactersView.element;
+        App.rootElement.appendChild(charactersElement);
+        hideCards();
+      } catch (error) {
+        console.warn(error);
+      } 
+    }
+}
+
+new App();
